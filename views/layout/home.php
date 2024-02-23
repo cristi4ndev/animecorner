67c7cac2c00919a0c9f7d0ea6def11b0a243fe82
Cristi4n.dev <cristian_msn91@hotmail.com>
2024-02-23 17:50:20 +0100

    grid home

--- views/layout/home.php
+++ views/layout/home.php
@@ -6,40 +6,42 @@
     <div class="homepage-grid">
         <div id="figuras">
             <img src="./uploads/images/home/figuras.jpg">
-            <h3>Figuras</h3>
         </div>
         <div id="goku">
             <img src="./uploads/images/home/goku.png">
-            <h3>Dragon Ball</h3>
+
         </div>
         <div id="onepiece">
             <img src="./uploads/images/home/onepiece.png">
-            <h3>One Piece</h3>
+        </div>
+        <div id="mochilas">
+            <img src="./uploads/images/home/mochilas.png">
+        </div>
+        <div id="funkos">
+            <img src="./uploads/images/home/funkos.png">
+        </div>
+        <div id="alfombrillas">
+            <img src="./uploads/images/home/alfombrillas.png">
         </div>
         <div id="camisetas">
             <img src="./uploads/images/home/camisetas.jpg">
-            <h3>Camisetas</h3>
         </div>
         <div id="sudaderas">
             <img src="./uploads/images/home/sudaderas.png">
-            <h3>Sudaderas</h3>
         </div>
         <div id="gorras">
             <img src="./uploads/images/home/gorras.jpg">
-            <h3>Gorras</h3>
         </div>
         <div id="disfraces">
             <img src="./uploads/images/home/cosplay.jpg">
-            <h3>Cosplay</h3>
         </div>
         <div id="tazas">
             <img src="./uploads/images/home/tazas.jpg">
-            <h3>Tazas</h3>
         </div>
         <div id="posters">
             <img src="./uploads/images/home/posters.jpg">
-            <h3>Posters</h3>
         </div>
+        
         <div></div>
     </div>
 
@@ -48,6 +50,15 @@
     <section class="products-list" id="last-added-products">
 
         <div class="product-listing">
+            <div>
+                <article class="article-list">
+                    <img src="<?= base_url ?>/uploads/images/bp18856_frente.jpg" class="article-list-image">
+                    <h3 class="article-list-name">Luffy</h3>
+                    <div class="article-list-price">
+                        <span>15</span><span class="currency">€</span>
+                    </div>
+                </article>
+            </div>
             <div>
                 <article class="article-list">
                     <img src="<?= base_url ?>/uploads/images/bp18856_frente.jpg" class="article-list-image">
