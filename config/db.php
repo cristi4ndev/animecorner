458e3de6bb8aa08e74b268a2d8a7d1a8b49adf37
Cristi4n.dev <cristian_msn91@hotmail.com>
2024-03-21 13:23:07 +0100

    change db pass

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 class Database {
     public static function connect() {
-        $db = new mysqli('50.31.176.102', 'mxzmwcti_admin', 'esp-195851', 'mxzmwcti_animecorner');
+        $db = new mysqli('50.31.176.102', 'mxzmwcti_animecorner', 'anime-1234', 'mxzmwcti_animecorner');
 		$db->query("SET NAMES 'utf8'");
         
 		return $db;
